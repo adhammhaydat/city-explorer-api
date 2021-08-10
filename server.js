6b920cf22ac449eb2dee8437948f191039744190
adhammhaydat <adhammohidat123@gmail.com>
2021-08-10 19:07:59 +0300

    solve back-end

--- server.js
+++ server.js
@@ -1,29 +1,55 @@
+'use strict'
 const express = require('express') // require the express package
-const app = express() // initialize your express app instance
-const cors = require('cors');
-app.use(cors()) // after you initialize your express app instance
+const server = express() // initialize your express app instance
+const cors = require('cors'); // after you initialize your express app instance
 require('dotenv').config();
 const PORT=process.env.PORT;
 const axios=require('axios');
+const weather=require('./data/weather.json');
+server.use(cors());
 // a server endpoint
- 
-app.get('/',(req,res)=>{
-    let searchQuery=req.query['q']
-    let url=`https://eu1.locationiq.com/v1/search.php?key=pk.5d26fb6c24e689ebb23ebfd89105b70b&q=${searchQuery}&format=json`
-    axios.get(url).then(data=>{res.json(data.data)})
-})
 
-// a server endpoint 
-app.get('/data',  (req, res)=> { // callback function of what we should do with our request
-  console.log(req.query);
-  if (req.query['q']==='hello'){
-    res.json({message:'301d28 students are awesome !'}).status(200)
-  }else{
-      res.status(400).send("something wrong happend");
-  }
-   // our endpoint function response
+server.listen(PORT,()=>{
+  console.log(`server is runiing in port ${PORT}`)
+})
+server.get('/',(req,res)=>{
+  res.send('hello adham')
+})
+server.get('/weather',(req,res)=>{               
+  res.send(weather)
+}); 
+
+
+
+server.get('/weather/:lon/:lat/:city_name',(req,res)=>{
+  const data= weather.find((element)=>+element.lon === +req.params.lon &&
+  +element.lat === +req.params.lat &&
+  element.city_name === req.params.city_name);
+  
+  if(data){res.send(data)
+  }else{res.send('the country not found')}
+  
+    
+  
 });
 
-app.listen(PORT,()=>{
-    console.log(`listening on port ${PORT}`);
-}); // kick start the express server to work
+server.get('/weather/:city_name',(req,res)=>{
+  let newWeather=[];
+  const findCity=weather.find((element)=>element.city_name === req.params.city_name);
+  if(findCity){
+    findCity.data.map((day)=>newWeather.push(new Forecast(day)));
+    res.send(newWeather);
+    
+  }else{
+    res.send('the location  was not found');
+  }
+})
+
+
+
+class Forecast {
+  constructor(city) {
+      this.date = city.datetime;
+      this.description = city.weather.description
+  }
+};
