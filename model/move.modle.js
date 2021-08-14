bbfef6db88b2042a3daaf6dc1be2b0694647c6c8
adhammhaydat <adhammohidat123@gmail.com>
2021-08-14 18:19:34 +0300

    solve lab9

--- model/move.modle.js
+++ model/move.modle.js
@@ -1,14 +1,14 @@
 'use strict';
 class Movies {
-  constructor(city){
+  constructor(movies){
 
-    this.title=city.title;
-    this.overview=city.overview;
-    this.average_votes=city.vote_average;
-    this.total_votes=city.vote_count;
-    this.image_url=`https://image.tmdb.org/t/p/w500/${city.poster_path}`;
-    this.popularity=city.popularity;
-    this.released_on=city.release_date;
+    this.title=movies.title;
+    this.overview=movies.overview;
+    this.average_votes=movies.vote_average;
+    this.total_votes=movies.vote_count;
+    this.image_url=`https://image.tmdb.org/t/p/w500/${movies.poster_path}`;
+    this.popularity=movies.popularity;
+    this.released_on=movies.release_date;
 
   }
 }
