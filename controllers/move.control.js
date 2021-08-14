ee878a7f534daa89c8a0f199a20ad565b63f6aa7
adhammhaydat <adhammohidat123@gmail.com>
2021-08-14 15:18:18 +0300

    solv lab9

--- controllers/move.control.js
+++ controllers/move.control.js
@@ -8,15 +8,16 @@ function moveController (req, response){
   let newMovies = []
   let url=`https://api.themoviedb.org/3/search/multi?api_key=${process.env.MOVIE_API_KEY}&query=${req.params.city_name}`
   let key=`movies${req.params.city_name}`
-  console.log(key)
+  
   if(newCache[key]&& Date.now()){
+    console.log(key)
     console.log("ok")
-      response.send(newCache[key].data);
+      response.send(newCache[key]);
   }else{
     axios.get(url).then(MoviesData => {
       MoviesData.data.results.forEach((ele)=> newMovies.push(new Movies(ele)));
       response.send(newMovies);
-      newCache[key].data=newMovies;
+      newCache[key]=newMovies;
     })
       .catch((error) => {
         console.log(error)
